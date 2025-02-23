6c8c7f46d35414eab6bee2b26820b75527aa906d
cszdiego <dcastillos04@outlook.com>
2025-02-23 15:29:36 -0600

    Pre entrenos acabado

---
+++ Primer parcial/productsData/vitaminas.js
@@ -0,0 +1,10 @@
+export const preWorkout = [
+    {
+        name: "C4",
+        image: "./Imagenes/Vitaminas/c4.jpg",
+        rating: 4.3,
+        precio: 699.45,
+        amzLink: "https://www.amazon.com.mx/Original-Lemonade-Preworkout-Extremadamente-Resistencia/dp/B0CSF5M463/ref=sr_1_1?crid=1YYUYXX7DQL73&dib=eyJ2IjoiMSJ9.2GnciKOKj2A5EJ8lmxakpaCJ5oRMlJb1H5ba9O3zomxNpAqYt6u5JgC2yCwoExkXE73S7etd66vm0C9P0GGzl-BRrVhCHMzwuLmCfg2MuFR27x1ANXFMkJ-ggcR2IB_DvHYC_2TKDPyAXiRCxLloaezka41Bnn1Y2nPNURdt9NNrrQcdVDe-xZ9dtw2CUf6y9zChaU-2RBPXy3Lw2xJre1IbcGxQK9UjHmHLLQXfKimn7Feq7TsdGphchUPuHHCrdhiUWg0LThnb27WytVpR11vwwFa4YZ5WHrbZn0YlfcPZvOGz8PnmdKv90QQ_eVh4kKqnWzG0c-CJFhS_4vggDkox8YkU9JqzzVx5VfLpugLX2N3vImjDf7uAhYRkaQmBHeQ3HP3K3tOvG54uwpyxUeUuXXXp5AUjEGBpSpybYJnctmtaIRR7rMuMNK4UBEhW.-4yVBNIFQA--4V9DfWslxR6eoIZp6okz0h79DDMVfuc&dib_tag=se&keywords=pre+entreno+c4&qid=1740344508&sprefix=pre+entren%2Caps%2C114&sr=8-1&ufe=app_do%3Aamzn1.fos.db4f1a57-87f1-43c5-9a39-0cdca6036b57",
+        gncLink: "https://gnc.com.mx/c4-pre-entrenador-cellucor-ponche-de-frutas-180-gramos.html",
+    },
+]
